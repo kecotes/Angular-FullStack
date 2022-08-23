a666583f044f800f632f5b7399287926e6eb3938
kecotes <kecotes@uniguajira.edu.co>
2022-08-23 18:05:01 -0500

    Change mode strict

---
+++ src/app/shared/teacher.ts
@@ -0,0 +1,11 @@
+export interface Teacher {
+    id: string;
+    name: string;
+    secondName: string;
+    status: string;
+    profession: string;
+    typeContract: string;
+    telephone:  string;
+    email:  string;
+    password:   string;
+}
