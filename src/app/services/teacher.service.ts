35d3d0e5822153b267e3a84d5c38a4e8d6c41da7
kecotes <kecotes@uniguajira.edu.co>
2022-08-23 18:33:57 -0500

    create service Teacher

---
+++ src/app/services/teacher.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Teacher } from '../shared/teacher';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class TeacherService {
+
+  baseurl = 'http://localhost:3000'; 
+  
+  constructor(
+    private http: HttpClient
+  ) { }
+  
+  // Http Headers, para que al usar la api sepa que son en archivos json
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  }
+
+  //POST
+  createTeacher(data): Observable<Teacher>{
+    return this.http.post<Teacher>(
+      this.baseurl + '/teachertracking', 
+      JSON.stringify(data), 
+      this.httpOptions
+        ).pipe
+          (retry(1), catchError(this.errorHandl));
+  } 
+
+  // GET
+  getTeacher(): Observable<Teacher> {
+    return this.http.get<Teacher>(
+      this.baseurl + '/teachertraking/'
+        ).pipe(retry(1), catchError(this.errorHandl));
+  }
+
+  // PUT
+  updateTeacher(id, data): Observable<Teacher>{
+    return this.http.put<Teacher>(
+      this.baseurl + '/teachertracking/' + id, 
+      JSON.stringify(data), 
+      this.httpOptions
+      ).pipe(retry(1), catchError(this.errorHandl));
+  }
+
+  // DELETE
+  deleteTeacher(id): Observable<Teacher>{
+    return this.http.delete<Teacher>(
+      this.baseurl + '/teachertracking/' + id,
+      this.httpOptions
+      ).pipe(retry(1), catchError(this.errorHandl));
+  }
+
+
+  // ERROR HANDLING 
+  errorHandl(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+  }
+
+}
--- src/app/shared/teacher.ts
+++ src/app/shared/teacher.ts
@@ -1,4 +1,4 @@
-export interface Teacher {
+export class Teacher {
     id: string;
     name: string;
     secondName: string;
